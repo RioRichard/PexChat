f4efaeab97ad6fc4947b03e11986b9aaf3ac8f1c
RioRichard <thanh.huuphungak47@gmail.com>
2022-07-19 20:43:50 +0700

    start project

---
+++ src/main/java/com/example/PexChat/PexChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.PexChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PexChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PexChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PexChat/PexChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PexChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PexChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
