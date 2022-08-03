6f2ac190d8304fb8dc3e5b856cdd9be9030314a3
Prako06 <anhkhoiphamtran21@gmail.com>
2022-08-03 14:53:18 +0700

    Xong Frontend Của Khoa

--- src/main/java/com/example/PexChat/Controller/AccountController.java
+++ src/main/java/com/example/PexChat/Controller/AccountController.java
@@ -8,7 +8,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -87,6 +89,10 @@ public class AccountController {
     // setting profile
     @RequestMapping("/accounts")
     String setting(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "loginPage";
+        }
         model.addAttribute("info",userService.GetCurrentUser());
         return "SettingPage";
     }
