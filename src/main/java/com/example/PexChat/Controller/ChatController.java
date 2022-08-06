ec6a7daa69552197f96f8c184dc84e0afd904fd3
Prako06 <anhkhoiphamtran21@gmail.com>
2022-08-06 16:16:42 +0700

    Prako

--- src/main/java/com/example/PexChat/Controller/ChatController.java
+++ src/main/java/com/example/PexChat/Controller/ChatController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,7 +25,8 @@ public class ChatController extends BaseController {
 
     
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("listRoom", roomService.getRooms());
         return "homepage";
     }
     
